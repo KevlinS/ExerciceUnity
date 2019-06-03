2fadb5d4eb4b9b928d00b17651768a947fadf077
KevlinS <kevlin.susanto@yahoo.com>
2019-06-03 17:17:24 +0200

    first commit

---
+++ Assets/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject ProjectilPrefab;
+    public GameObject Spawner;
+    public float Power;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            GameObject g = Instantiate(ProjectilPrefab,Spawner.GetComponent<Transform>().position,Quaternion.identity);
+            g.tag = "Projectile";
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            g.GetComponent<Rigidbody>().AddForce(fwd * Power);
+            Destroy(g,5);
+        }
+    }
+}
